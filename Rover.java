1ca594fd885141cc3ca82481c11a583f29ce73aa
Albano-de-Souza <99596573+Albano-de-Souza@users.noreply.github.com>
2026-09-03 19:32:00 -0300

    feat: adiciona script de inicializacao do rover

---
+++ Rover.java
@@ -0,0 +1,12 @@
+// Rover.java
+public class Rover {
+    public static void inicializarRover() {
+        System.out.println("Sistemas do Rover iniciados!");
+        System.out.println("Painéis solares: OK");
+        System.out.println("Nivel de bateria: 100%");
+    }
+
+    public static void main(String[] args) {
+        inicializarRover();
+    }
+}
